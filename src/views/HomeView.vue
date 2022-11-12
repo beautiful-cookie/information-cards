d517c5a747975337a7edb45747b4fb47900c1175
beautiful-cookie <maxim.khadeev@gmail.com>
2022-11-12 23:31:04 +0300

    card view method change

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,39 @@
 <template> 
   <div class="content-wrapper">
 
-    <div class="card">
-      <div class="category-name-wrapper"><span class="empty-class"></span><span class="category-name">{Категория}</span></div>
-      <div class="image-wrapper"><img src="@/assets/testImg.jpg" alt="Ops.."></div>
-      <div class="title-wrapper"><span class="title">{Название}</span></div>
-      <div class="description">{Описание}</div>
-      <div class="url">{Url}</div>
-    </div>
+    <details>
+      <div>
+        <div class="card">
+          <div class="category-name-wrapper"><span class="empty-class"></span><span class="category-name">{Категория}</span></div>
+          <div class="image-wrapper"><img src="@/assets/testImg.jpg" alt="Ops.."></div>
+          <div class="title-wrapper"><span class="title">{Название}</span></div>
+          <div class="description">{Описание}</div>
+          <div class="url">{Url}</div>
+        </div>
+      </div>
+
+      <summary> 
+        Название 1
+      </summary>
+    </details>
+
+    
+    <details>
+      <div>
+        <div class="card">
+          <div class="category-name-wrapper"><span class="empty-class"></span><span class="category-name">{Категория}</span></div>
+          <div class="image-wrapper"><img src="@/assets/testImg.jpg" alt="Ops.."></div>
+          <div class="title-wrapper"><span class="title">{Название}</span></div>
+          <div class="description">{Описание}</div>
+          <div class="url">{Url}</div>
+        </div>
+      </div>
+
+      <summary> 
+        Название 2 
+      </summary>
+    </details>
+
 
   </div>
 </template>
@@ -19,8 +45,30 @@
   align-items: center; 
   flex-direction: column; 
   width: 100vw; 
-  gap: 10px; 
   margin-top: 50px; 
+  gap: 20px; 
+
+
+  details div {
+    display: flex; 
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    cursor: pointer; 
+    user-select: none;
+  }
+
+  summary {
+    list-style: none; 
+    color: #747474; 
+    cursor: pointer; 
+    transition-property: color;
+    transition-duration: 0.3s;
+
+    &:hover {
+      color: #c0c0c0;
+    }
+  }
 
   .card {
     display: flex; 
@@ -31,10 +79,10 @@
     padding: 10px; 
     border-radius: 10px; 
     box-shadow: 0 0 4px white; 
-    min-width: 40%; 
-    max-width: 60%;
-    max-height: 60vh;
+    max-width: 40%;
+    max-height: 60%;
     gap: 10px; 
+    margin-top: 20px;
     
     .category-name-wrapper {
       display: flex; 
@@ -71,32 +119,20 @@
       border-radius: 5px; 
 
       img {
-        width: 100%; 
-        height: 100%; 
+        max-width: 100%; 
+        max-height: 100%; 
       }
     }
   } 
 
-  @media screen and (max-width: 1000px) {
-
+  @media screen and (max-width: 700px) {
     .card {
-      min-width: 20%; 
-      max-width: 40%;
-      max-height: 60vh;
-    } 
+      max-width: 60%;
+      max-height: 60%; 
+    }
   }
 }
 
-@media screen and (max-width: 700px) {
 
-  .card {
-    min-width: 40%; 
-    max-width: 60%;
-    max-height: 60vh;
-  }
-  .image-wrapper {
-    
-  }
-}
 
 </style>
